ad85d5523f23cbe056e9c862d4f78231ee5abf05
HarDev97 <har.rod.pe@outlook.com>
2023-12-29 18:02:40 -0500

    Métodos GET,POST,PUT y DELETE v02

--- api_python/venv/bin/activate.fish
+++ api_python/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/hs/api_python/venv"
+set -gx VIRTUAL_ENV "/home/hs/python/api_python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
