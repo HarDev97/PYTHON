4e2e450783b321da813593cbe2e411ec46810205
HarDev97 <har.rod.pe@outlook.com>
2024-01-01 18:28:38 -0500

    Declarando boleanos
    Invirtiendo valor boleano con not

---
+++ fundamentos_python/boleanos.py
@@ -0,0 +1,15 @@
+# Declarando boleanos
+is_single = True
+print(type(is_single))
+print(is_single)
+
+is_single = False
+print(is_single)
+
+# Invertir valor con not
+
+print(not True)
+print(not False)
+
+is_single = not is_single
+print(is_single)
