2f6b787fd17fe1f4f7f1c67daaa17f2e79bd4015
HarDev97 <har.rod.pe@outlook.com>
2024-01-01 17:44:21 -0500

    Tipos de datos
    str,int,bool,float

---
+++ fundamentos_python/tipos_datos.py
@@ -0,0 +1,17 @@
+# Cadena de caracteres str
+my_name = "William"
+print(my_name)
+# Imprimiendo que tipo de dato es type()
+print("La variables es de tipo", type(my_name))
+
+# Enteros int
+my_age = 20
+print("La variables es de tipo", type(my_age))
+
+# Boleano bool
+is_single = True
+print("La variables es de tipo", type(is_single))
+
+# Flotante float
+score = 9.8
+print("La variables es de tipo", type(score))
