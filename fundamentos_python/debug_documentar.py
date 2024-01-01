b52621a35662fbd65047e7d8b332e122865cac12
HarDev97 <har.rod.pe@outlook.com>
2024-01-01 17:22:05 -0500

    Debug con print y formas de documentar

---
+++ fundamentos_python/debug_documentar.py
@@ -0,0 +1,15 @@
+# Imprimir mensaje
+print("Hola")
+
+
+# Imprimir cálculos
+"""
+Se realizan diferentes operaciones
+"""
+"""
+Se realizan diferentes operaciones
+"""
+print(12 * 5)
+print(12 / 5)
+print(10 - 6)
+print(10 + 6)
