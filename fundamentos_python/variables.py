194fd32daed573f1fe8ebf233352e3712a72e7d3
HarDev97 <har.rod.pe@outlook.com>
2024-01-01 17:36:43 -0500

    Declarando e imprimiendo variables
    Funciones implementadas print, input

---
+++ fundamentos_python/variables.py
@@ -0,0 +1,16 @@
+# Declarando variables
+my_name = "Har"
+print(my_name)
+
+my_age = 10
+print(my_age)
+
+my_name = "HarAl"
+print(my_name)
+
+# Concatenando mensaje con variable
+print("El nombre ha cambiado por", my_name)
+
+# Solicitando variables
+my_name = input("Digite su nombre: ")
+print("El nombre digitado es", my_name)
