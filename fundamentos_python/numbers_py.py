6331c4c9ada696ec4efd15783bc8f9404fd40e2d
HarDev97 <har.rod.pe@outlook.com>
2024-01-01 18:16:25 -0500

    Declarando variables numéricas
    Abreviación de sintaxis incrementar += decrementar -=

---
+++ fundamentos_python/numbers_py.py
@@ -0,0 +1,44 @@
+# Declarando números
+
+lives = 3
+age = 20
+print(type(lives))
+
+temperature = 12.12
+print(type(temperature))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+# Asigando variable con operación
+lives = 5
+lives = lives - 1
+print(lives)
+
+# Decrementar variable (sintaxis reducida)
+lives = 5
+lives -= 1
+print(lives)
+
+# Incrementar variable (sintaxis reducida)
+lives = 5
+lives += 1
+print(lives)
+
+# Anotación científica
+number = 450000000000000000000000.1
+print(number)
+
+number = 0.000000000000000000000001
+print(number)
+
+# Promedio
+buget_1 = 50
+buget_2 = 100
+buget_3 = 40
+
+total_buget = buget_1 + buget_2 + buget_3
+avg = total_buget / 3
+print(avg)
